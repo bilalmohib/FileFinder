e798a1d71143f7dbb49e9022cb2ff716fed6535d
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-10-17 14:00:31 +0500

    Almost done

---
+++ SELABPROJECT_SEARCHING/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SELABPROJECT_SEARCHING
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(324, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(111, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 138);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SELABPROJECT_SEARCHING/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SELABPROJECT_SEARCHING
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string[] dirs = Directory.GetDirectories(@"E:\Fifth Semester Material\AI Lab\Python\Assignment\Assignment 2 Solving Problem by Searching", "*", SearchOption.TopDirectoryOnly);
+                //MessageBox.Show("The number of directories starting with p is {0}.", dirs.Length.ToString());
+                //foreach (string dir in dirs)
+                //{
+                //    MessageBox.Show(dir);
+                //}
+
+                //To search within a file
+                //string[] dirs = (string[])Directory
+                //.EnumerateFiles(@"E:\Upwork Projects Clients\Marcello\WebOffice Tool\src\app\main\pages\profile", "*.jsx")              // all txt files (put the right wildcard)
+                //.Where(file => File
+                //.ReadLines(file)                            // with at least one line
+                //.Any(line => line.Contains("Select the user about which you want to set permissions for pages")))  // which contains stringToFind
+                //.ToArray();
+                //MessageBox.Show(dirs[0].ToString());
+
+
+                //string rootdir = @"E:\Upwork Projects Clients\Marcello\WebOffice Tool\src\app\main\pages\profile";
+                //string[] files = Directory.GetFileSystemEntries(rootdir, "*", SearchOption.AllDirectories);
+                //MessageBox.Show(String.Join(Environment.NewLine, files));
+
+                var textBoxText = textBox1.Text;
+
+                string rootdir = @textBoxText;
+
+                //// print list of files in the root directory and all its subdirectories
+                //var files = Directory.EnumerateFiles(rootdir, "*", SearchOption.AllDirectories);
+                //MessageBox.Show(String.Join(Environment.NewLine, files));
+
+                // print list of directories and subdirectories
+                var dirs = Directory.EnumerateDirectories(rootdir, "*", SearchOption.AllDirectories);
+ //             MessageBox.Show(dirs.GetType().Name);
+                for (int i = 0; i < dirs.Count(); i++)
+                {
+                    string str1 = dirs.ElementAt(i);
+                    MessageBox.Show("The string is at: "+str1);
+                    string[] dirs2 = (string[])Directory
+                    .EnumerateFiles(@str1, "*.jsx")              // all txt files (put the right wildcard)
+                    .Where(file => File
+                    .ReadLines(file)                            // with at least one line
+                    .Any(line => line.Contains("Select the user about which you want to set permissions for pages")))  // which contains stringToFind
+                    .ToArray();
+                    foreach (string dir in dirs2)
+                    {
+                        MessageBox.Show("The path is found in directory "+dir);
+                    }
+                    // do your stuff   
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("The process failed: {0}", E.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
