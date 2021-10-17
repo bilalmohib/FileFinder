dfb59323c4c7e4835f6f8785d08ec248e344db28
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-10-17 15:03:59 +0500

    feat

--- Software Code/SELABPROJECT_SEARCHING/Form1.Designer.cs
+++ Software Code/SELABPROJECT_SEARCHING/Form1.Designer.cs
@@ -36,12 +36,17 @@ namespace SELABPROJECT_SEARCHING
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(320, 387);
+            this.button1.Location = new System.Drawing.Point(309, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 39);
             this.button1.TabIndex = 0;
@@ -52,7 +57,7 @@ namespace SELABPROJECT_SEARCHING
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(36, 75);
+            this.textBox1.Location = new System.Drawing.Point(36, 112);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(706, 33);
             this.textBox1.TabIndex = 1;
@@ -62,7 +67,7 @@ namespace SELABPROJECT_SEARCHING
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(26, 35);
+            this.label1.Location = new System.Drawing.Point(27, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(730, 37);
             this.label1.TabIndex = 2;
@@ -92,7 +97,7 @@ namespace SELABPROJECT_SEARCHING
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(36, 190);
+            this.textBox3.Location = new System.Drawing.Point(36, 206);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(706, 33);
             this.textBox3.TabIndex = 4;
@@ -101,17 +106,74 @@ namespace SELABPROJECT_SEARCHING
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(26, 150);
+            this.label3.Location = new System.Drawing.Point(27, 166);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(729, 37);
             this.label3.TabIndex = 5;
             this.label3.Text = "Enter the File Extension in which you want to find the string :";
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(616, 413);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 15);
+            this.linkLabel1.TabIndex = 6;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.linkLabel2.Location = new System.Drawing.Point(298, 453);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(476, 32);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/Muhammad-Bilal-7896/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(136, 455);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Contact Me at :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(279, 413);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(257, 30);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Made By Muhammad Bilal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label6.Location = new System.Drawing.Point(309, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 43);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "File Finder";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(855, 494);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
@@ -120,7 +182,8 @@ namespace SELABPROJECT_SEARCHING
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Tag = "";
+            this.Text = "FileFinder";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,6 +198,11 @@ namespace SELABPROJECT_SEARCHING
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
